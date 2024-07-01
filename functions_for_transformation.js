2d7616a4f4e9292b5774572249e098e15a3fee99
Ted Yu <tedhyu@yahoo.com>
2024-07-01 16:26:18 -0700

    Create functions_for_transformation.js
    Start of coding transformation function

---
+++ functions_for_transformation.js
@@ -0,0 +1,11 @@
+//This document will contain necessary function for the game.  by tedhyu.  Feel free to modify and I can explain to you.
+
+/*xy_to_hex
+input:  the x and y location of the mouse pad.
+
+output:  three integers array in the format of the hexagon location
+        [a,b,c]
+        a:  the index of the hexagon 
+
+
+function xy_to_hex(x,y):
