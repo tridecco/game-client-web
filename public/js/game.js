5bfb0ea679eeb394bd31c56755b4c5ae1d57a729
Tony Kan <tianchengkan@gmail.com>
2024-08-02 17:57:01 -0700

    feat: Add GameUI class and update game page
    - Added `GameUI` class to manage visibility of game sections (`queue`, `room`, `game`).
    - Updated `game.js` to use `GameUI` for showing and hiding sections.
    - Implemented script to handle game start parameters from the URL and show appropriate sections.

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,70 @@
 /**
  * Game modules and classes.
- * @module js/game
  */
+
+/**
+ * Game UI class.
+ * @module js/game/ui
+ */
+class GameUI {
+  /**
+   * Get a section. (Private)
+   * @param {string} sectionName - The section name.
+   * @returns {HTMLElement} The section.
+   */
+  _getSection(sectionName) {
+    if (
+      sectionName !== "queue" &&
+      sectionName !== "room" &&
+      sectionName !== "game"
+    ) {
+      throw new Error("Invalid section name");
+    }
+    return document.getElementById(sectionName);
+  }
+
+  /**
+   * Show a section. (Private)
+   * @param {string} section - The section name.
+   */
+  _showSection(section) {
+    this._getSection(section).style.display = "block";
+  }
+
+  /**
+   * Hide a section. (Private)
+   * @param {string} section - The section name.
+   */
+  _hideSection(section) {
+    this._getSection(section).style.display = "none";
+  }
+
+  /**
+   * Show a section, hide the others.
+   * @param {string} section - The section name.
+   */
+  showSection(section) {
+    const sections = ["queue", "room", "game"];
+    sections.forEach((s) => {
+      if (s === section) {
+        this._showSection(s);
+      } else {
+        this._hideSection(s);
+      }
+    });
+  }
+
+  /**
+   * Show the queue.
+   */
+  showQueue() {
+    this.showSection("queue");
+  }
+
+  /**
+   * Show the room.
+   */
+  showRoom() {
+    this.showSection("room");
+  }
+}
