589035430f81fa8d2c9f93585c8f0c5511f6523f
Tony Kan <tianchengkan@gmail.com>
2025-04-20 14:44:31 -0700

    feat(server): Add main server file with configuration and initialization

---
+++ server.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview Server Main File
+ * @description Server configuration and initialization.
+ */
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Load required modules
+const express = require('express');
+
+const DEFAULT_PORT = 3000;
+const port = process.env.PORT || DEFAULT_PORT;
+const host = process.env.HOST || 'localhost';
+
+const app = express();
+
+// Load public files
+app.use(express.static('public'));
+
+// Load the renderer (EJS)
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// Start the server
+app.listen(port, host, () => {
+  console.log(
+    `Tridecco Game Client (Web) Server is running on http://${host}:${port}`,
+  );
+});
+
+module.exports = app;
