a802575d382471725973daf444c5ab66e83e3f30
Tony Kan <tianchengkan@gmail.com>
2025-04-19 19:37:01 -0700

    chore(eslint): Add ESLint configuration file with custom rules

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    curly: ['error', 'multi-line'],
+    eqeqeq: ['error', 'always'],
+    'no-duplicate-imports': 'error',
+    'no-magic-numbers': ['error', { ignore: [0, 1] }],
+    'consistent-return': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-unused-vars': 'warn',
+    'no-shadow': 'error',
+    'func-names': 'error',
+    'prefer-arrow-callback': 'error',
+  },
+  ignores: ['**/*.test.js', '**/dist/**', '**/maps/**'],
+};
