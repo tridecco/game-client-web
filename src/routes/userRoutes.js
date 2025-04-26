4ac4fd16ab917f5f0b3e7984991f924a7aa0f702
Tony Kan <tianchengkan@gmail.com>
2025-04-26 16:35:51 -0700

    feat(user): Update user routes to render edit profile page

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,8 +6,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.render('pages/user/profile');
+router.get('/edit', (req, res) => {
+  return res.render('pages/user/edit-profile');
 });
 
 module.exports = router;
