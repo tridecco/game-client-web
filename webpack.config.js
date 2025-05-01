da623572e6fb99ae76e02dad1d90e56e28337269
Tony Kan <tianchengkan@gmail.com>
2025-04-30 20:41:11 -0700

    feat(webpack): Add initial webpack configuration for production/development build

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const fs = require('fs');
+
+module.exports = {
+  mode: 'production',
+  entry: () => {
+    const files = fs
+      .readdirSync(path.resolve(__dirname, 'js'))
+      .filter((file) => file.endsWith('.js'))
+      .reduce((entries, file) => {
+        entries[file] = path.resolve(__dirname, 'js', file);
+        return entries;
+      }, {});
+
+    return files;
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: '[name]',
+  },
+
+  optimization: {
+    minimizer: [
+      ...(process.env.NODE_ENV === 'production'
+        ? [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true,
+                },
+              },
+              extractComments: false,
+            }),
+          ]
+        : []),
+    ],
+  },
+};
